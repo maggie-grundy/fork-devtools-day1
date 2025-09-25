75b0c097056e355ee4fdbe1a5205176db7f2aa40
Maggie Grundy <maggie.grundy@gmail.com>
2025-09-25 18:04:08 -0500

    Update html with new button, debug JavaScript, add CSS for new button

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # devtools-day1
+
+This is a fork project to test javascript and debug
+
+## How to Run this
+1. Clone the repository
+```bash
+    git clone repo_url
+```
+
+2. Move into the folder
+```bash
+    cd repo_name
+```
+
+## Git useful tips
+1. Create a new branch
+```bash
+    git checkout -b new_branch_name
+```
--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 // Error 1:
 let message = 'Welcome to the debugging exercise!';
-console.log(mesage); 
+console.log(message); 
 
 // Error 2: 
-let button = document.querySelector('.actionButton'); 
+//needs a variable defined?
+console.log(document);
+
+//Everything is an OBJECT in JS - represent objects using dot.notation
+/*Different ways of accessing elements of the document~
+  1. getElementById
+  2. getElementsByClassName
+  3. getElementsByTagName
+  4. querySelector
+  5. querySelectorAll
+  6. I swear I'm adding these comments myself haha
+*/
+
+
+let button = document.querySelector('#actionButton'); 
 button.addEventListener('click', eventHandler); 
 
 // Error 3: 
@@ -18,19 +32,33 @@ function displayOutput() {
   output.innerText = 'You clicked the button!';
 }
 
+let localMessage = 'Initial message';
+
+//where did error 5 go
+
 // Error 6: Misusing variable scope
 function updateMessage() {
-  let localMessage = 'Updated message';
+  localMessage = 'Updated message';
 }
+
 updateMessage();
 console.log(localMessage); 
 
-// Error 7:
-document.getElementById('testButton').addEventListener('click', testFunction());
+// Error 7: same as before without creating & storing variable 
+//I couldn't get it to work - the message appears before clicking the button
+document.getElementById('testButton').addEventListener('click', function testFunction() {
+  console.log('Imposter Button Clicked!');
+}); 
 
-function testFunction() {
-  console.log('Test function executed');
+/*I tried looking up how to fix this and found suggestions to wrap it within a function,
+I saw a syntax like this and tried it but not 100% sure how/why it works. I think it's because
+it creates a new scope for the function so it doesn't interfere with other functions?*/
+
+/*scrapped
+ function testFunction() {
+   console.log('Imposter Button Clicked!');
 }
+*/
 
 // Error 8:
 let number1 = 10;
@@ -39,33 +67,55 @@ console.log('Sum is: ' + (number1 - number2));
 
 // Error 9: 
 let nullVar = null;
-console.log(nullVar.length);
+console.log(nullVar);
 
 // Error 10:
 let caseSensitiveVar = 'Case matters';
-console.log(CaseSensitiveVar);
+console.log(caseSensitiveVar);
 
 // Error 11:
 let importantData =
   "This string must be logged to the console. It's important!";
-console.log('importantData');
+console.log(importantData);
 
 // Error 12: 
-tryToCallFunction();
+//not defined
+//tryToCallFunction();
 
 // Error 13:
-let user = { namee: 'Alice' };
+let user = { 
+  name: 'Alice',
+  age: 99  
+};
 console.log(user.name)
 
 // Error 14: 
 let someNumber = 123;
-console.log(someNumber.toUpperCase()); 
+console.log(someNumber);
 
-// Error 15: 
-let greeting = Hello, world!;
+let textToUpper = 'something to upper';
+//toUpperCase only works with string value
+console.log(textToUpper.toUpperCase());
+
+// Error 15:
+/* There are 3 types of variables
+  1. let -> can update value
+  2. var -> can update value
+  3. const -> cannot update values
+*/
+
+// let numbers = 123;
+// let strings ="eqjrqt";
+// let booleans = true;
+// let nulls = null;
+//let undefineds = undefined;
+//let objects = {};
+//let arrays = [];
+
+let greeting = "Hello, world!";
 
 // Error 16: 
-for (let i = 0; i < 5; i++); {
+for (let i = 0; i < 5; i++) {
     console.log(i);
 }
 
@@ -75,21 +125,28 @@ if (five === 5) {
     console.log('Five is equal to 5'); // This one should be logged
 } else {
     console.log('Five is not equal to 5'); 
+}
 
 // Error 18:
 Array.prototype.push = function() {
     console.log('Array push method is overwritten');
 };
+
 let numbers = [];
 numbers.push(1);
 console.log(numbers);
 
 // Error 19:
-console.log(x);
 x = 5;
+console.log(x);
+
 
 // Error 20:
 let myString = 'Hello';
-myString.property = 'World';
-console.log(myString.property); 
+console.log(myString); 
 
+let myStringObject = {
+  text1: "Goodbye World"
+};
+
+console.log(myStringObject.text1);
